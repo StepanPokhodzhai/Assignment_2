c6dfc2f97ac3d48f899d69cab9942eef13de290b
StepanPokhodzhai <stelkerstepan@gmail.com>
2023-01-10 20:53:51 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	base   float64
+	height float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+func main() {
+	tr := triangle{1.1, 2.2}
+	sq := square{3.3}
+
+	printArea(tr)
+	printArea(sq)
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
